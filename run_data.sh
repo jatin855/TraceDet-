5b449d09e9234e3ceee1a1e93d3c7e628763178a
jatin kushwaha <jatinkushwaha430@gmail.com>
2026-07-04 15:17:55 +0530

    update gen_length

--- run_data.sh
+++ run_data.sh
@@ -6,9 +6,9 @@ cd ~/TraceDet-
 
 
 
-CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task train --generate_task entropy --gen_length 64 > logs/triviaqa_train_ent.log 2>&1
-CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task val --generate_task entropy --gen_length 64 > logs/triviaqa_val_ent.log 2>&1
-CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task test --generate_task entropy --gen_length 64 > logs/triviaqa_test_ent.log 2>&1
+CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task train --generate_task entropy --gen_length 128
+CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task val --generate_task entropy --gen_length 128
+CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task test --generate_task entropy --gen_length 128
 
 
 
