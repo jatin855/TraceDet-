ea27864d32ed22f9cb962d61401680728072a0bd
jatin kushwaha <jatinkushwaha430@gmail.com>
2026-07-06 13:18:40 +0530

    fix in main_pipeline

--- run_data.sh
+++ run_data.sh
@@ -3,7 +3,7 @@ cd ~/TraceDet-
 
 
 
-CUDA_VISIBLE_DEVICES=1 python -m DLM_generate.generate_interoutput --dataset triviaqa --task train --generate_task entropy --gen_length 128 > logs/triviaqa_train_ent.log 2>&1
+#CUDA_VISIBLE_DEVICES=1 python -m DLM_generate.generate_interoutput --dataset triviaqa --task train --generate_task entropy --gen_length 128 > logs/triviaqa_train_ent.log 2>&1
 CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task val --generate_task entropy --gen_length 128 > logs/triviaqa_val_ent.log 2>&1
 CUDA_VISIBLE_DEVICES=0 python -m DLM_generate.generate_interoutput --dataset triviaqa --task test --generate_task entropy --gen_length 128 > logs/triviaqa_test_ent.log 2>&1
 
